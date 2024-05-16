d6d030e168ce54ebacc1b6df843c737f978489be
Srikanth Melkote Srinivasan <30727982+srikanth24093233@users.noreply.github.com>
2024-05-16 10:41:51 -0400

    Initial commit

---
+++ src/main/java/com/example/tripwallet/TripWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tripwallet/TripWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
