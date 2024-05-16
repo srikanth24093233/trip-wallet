a47ed3956f10726ea103bf12fb3a79aabc6b82ec
Srikanth Melkote Srinivasan <30727982+srikanth24093233@users.noreply.github.com>
2024-05-16 14:54:57 -0400

    qr reader

--- src/main/resources/static/scripts/scripts.js
+++ src/main/resources/static/scripts/scripts.js
@@ -6,3 +6,29 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText, decodeResult);
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
